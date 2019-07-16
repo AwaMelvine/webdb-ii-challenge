eff6aff3d355e1831d21915300e3e05797c4f547
Awa Melvine <melvineawa@gmail.com>
2019-07-16 14:58:02 +0100

    Create model method to update cars

--- models/Car.js
+++ models/Car.js
@@ -3,5 +3,10 @@ const db = require("../data/db");
 module.exports = {
     get() {
         return db("cars");
-    }
+    },
+
+    async update(id, data) {
+        const count = await db('cars').where('id', id).update(data);
+        return (count > 0 ? this.get(id) : null);
+    },
 }
